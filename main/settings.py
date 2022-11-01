309ff0022efe30cb1fcae9a25e1a208a3e4c3968
Henrry G <henrrygc@aol.com>
2022-11-01 20:33:44 +0100

    added security dependencies

--- main/settings.py
+++ main/settings.py
@@ -38,6 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # third_party apps
+
+    # my apps
+
 ]
 
 MIDDLEWARE = [
@@ -117,6 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
